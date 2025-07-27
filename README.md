f7223e4656ac7fd7f3e6c452f8aee57da1e636bd
Justin <jay_stefan@gmail.com>
2025-07-26 19:10:06 -0700

    added delete and put routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://medium.com/@kevinkoech265/a-guide-to-connecting-postgresql-and-pythons-fast-api-from-installation-to-integration-825f875f9f7d
+
+pip install -r requirements.txt
--- router/posts.py
+++ router/posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 import schemas
@@ -16,11 +16,33 @@ def test_posts(db: Session = Depends(get_db)):
     post = db.query(models.Post).all()
     return post
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=List[schemas.CreatePost])
-def create_post(post_post:schemas.CreatePost, db:Session = Depends(get_db)):
-    new_post = models.Post(**post_post.model_dump())
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.CreatePost)
+def create_post(post: schemas.CreatePost, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
-    db.refresh
+    db.refresh(new_post)  # Refresh to get the updated object with DB-assigned fields
 
-    return [new_post]
+    return new_post
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int, db:Session = Depends(get_db)):
+    delete_post = db.query(models.Post).filter(models.Post.id == id)
+    
+    if delete_post.first() is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"The id: {id} you requested does not exist")
+    delete_post.delete(synchronize_session=False)
+    db.commit()
+
+@router.put('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.CreatePost)
+def update_post(update_post:schemas.PostBase, id:int, db:Session = Depends(get_db)):
+    updated_post = db.query(models.Post).filter(models.Post.id == id)
+
+    if updated_post.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post ID: {id} does not exist")
+    updated_post.update(update_post.model_dump(), synchronize_session=False)
+    db.commit()
+    
+    return updated_post.first()
